91616fc1e797bd6d78df4023b14dd719bf74fd10
saifaldin14 <saifaldindeveloper@gmail.com>
2021-09-30 18:01:10 -0400

    Create Server project

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,40 @@
 //Create plan for how we will structure the server app
+// Create basic template project
+// Define Notes Data Structure
+// Define GET Method
+// Define POST Method
+// Define PIN Method
+// Define UNPIN Method
+// Define CLEAR Method
+// Define SHAKE Method
+// Define DISCONNECT Method
+import java.io.* ;
+import java.net.* ;
+import java.util.* ;
 
-public class Server {
+public final class Server {
+    ServerSocket serverSocket;
+    ArrayList<ServerConnection> connections = new ArrayList<ServerConnection>();
+    boolean shouldRun = true;
+
+    public static void main(String[] args) {
+        new Server();
+    }
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(3333);
+            while (shouldRun) {
+                Socket socket = serverSocket.accept();
+                ServerConnection serverConnection = new ServerConnection(socket, this);
+                serverConnection.start();
+                connections.add(serverConnection);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+
